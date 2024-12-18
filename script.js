60998d03e65fd6546696fd206f08c4e1de0c8dd3
Ylenia <yleniamighela@gmail.com>
2024-12-18 15:04:47 +0100

    [feat] correct order logic

--- script.js
+++ script.js
@@ -6,44 +6,37 @@
 
 for(let i = 1; i <= 100; i++) {
     
- // ---SE sono multipli di 3 
-    if (i % 3 === 0) {
-            
-    //  OUTPUT
-    //stamperà Fizz
-    console.log("Fizz");
+    // ---SE sono multipli sia di 3 che di 5 
+    if(i % 3 === 0 && i % 5 === 0) {
+        //     OUTPUT
+        console.log("FizzBuzz");
+        
     }
-
-
-
-// ---SE sono multipli di 5 
- else if(i % 5 === 0) {
-//     OUTPUT
-//     stamperà Buzz
-console.log("Buzz");
-
- }
-
-
-
-// ---SE sono multipli sia di 3 che di 5 
-else if(i % 3 === 0 && i % 5 === 0) {
-//     OUTPUT
-//     stamperà FizzBuzz
-console.log("FizzBuzz");
-
-}
-
-// ---ALTRIMENTI SE non sono multipli
-else {
-    //     OUTPUT
-//     stamperà il numero di quel punto di iterazione
-
- console.log(i);
- 
-}
     
-
+    // ---SE sono multipli di 3 
+    else if (i % 3 === 0) {
+        //  OUTPUT
+        console.log("Fizz");
+    }
+    
+    
+    
+    // ---SE sono multipli di 5 
+    else if(i % 5 === 0) {
+        //     OUTPUT
+        console.log("Buzz");
+        
+    }
+  
+    
+    // ---ALTRIMENTI SE non sono multipli
+    else {
+      //     OUTPUT     
+        console.log(i);
+        
+    }
+    
+    
 }
 
 
